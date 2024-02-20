ff9fd068a978e2745d3675aa97e6296d892443e3
Victor Gyozo Vancsura <gyozovancsura@gmail.com>
2024-02-20 16:49:09 +0000

    started implementing edits on profile

--- src/APIcalls/getuser.js
+++ src/APIcalls/getuser.js
@@ -1,10 +1,21 @@
 import axios from "axios";
 
-export default async function getUser(username, setUserDetails, setIsLoggedin) {
-  const response = await axios.get(
-    `https://tipper-api-xzkf.onrender.com/waiter/${username}`
-  );
-  const { waiter } = response.data;
-  setUserDetails(waiter);
-  setIsLoggedin(true);
+export default async function getUser(
+  username,
+  setUserDetails,
+  setIsLoggedin,
+  setLoginError
+) {
+  try {
+    const response = await axios.get(
+      `https://tipper-api-xzkf.onrender.com/waiter/${username}`
+    );
+    const { waiter } = response.data;
+    setUserDetails(waiter);
+    setIsLoggedin(true);
+  } catch (error) {
+    if (error.response.data.msg === "Not found") {
+      setLoginError({ type: "username", msg: "Username does not exist" });
+    }
+  }
 }
