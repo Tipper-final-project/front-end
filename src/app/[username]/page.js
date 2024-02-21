0af44cbb5a34e7723ed53b7bc8efbe9cfc696fb7
avr87 <annavanruiten@gmail.com>
2024-02-21 16:14:28 +0000

    added qr-code & stripe page with styling and added icon to Header

--- src/app/[username]/page.js
+++ src/app/[username]/page.js
@@ -1,11 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import  getUser  from "../../APIcalls/getuser"
+import getUser from "../../APIcalls/getuser";
 import { loadStripe } from "@stripe/stripe-js";
 import {
   EmbeddedCheckoutProvider,
   EmbeddedCheckout,
 } from "@stripe/react-stripe-js";
+import Header from "../Components/Header";
+import "../../../src/output.css";
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
@@ -14,12 +16,12 @@ const stripePromise = loadStripe(
 export default function App({ params }) {
   const [clientSecret, setClientSecret] = useState("");
   const [userDetails, setUserDetails] = useState("");
- 
-  const username = params.username
+
+  const username = params.username;
   console.log(userDetails);
 
   useEffect(() => {
-    getUser(username, setUserDetails)
+    getUser(username, setUserDetails);
     fetch("/api", {
       method: "POST",
     })
@@ -29,17 +31,26 @@ export default function App({ params }) {
 
   return (
     <>
-    <div id="checkout">
-      <h1>{userDetails.username}'s Profile</h1>
-      <p>{userDetails.bio}</p>
-      {clientSecret && (
-        <EmbeddedCheckoutProvider
-          stripe={stripePromise}
-          options={{ clientSecret }}
-        >
-          <EmbeddedCheckout />
-        </EmbeddedCheckoutProvider>
-      )}
-    </div>
-  </>);
+      <Header />
+      <div className="checkout-profile">
+        <img
+          className="checkout-profile-pic"
+          src={userDetails.img}
+          alt="profile-picture"
+        ></img>
+        <h1>{userDetails.username}'s Profile</h1>
+        <p>{userDetails.bio}</p>
+      </div>
+      <div id="checkout">
+        {clientSecret && (
+          <EmbeddedCheckoutProvider
+            stripe={stripePromise}
+            options={{ clientSecret }}
+          >
+            <EmbeddedCheckout />
+          </EmbeddedCheckoutProvider>
+        )}
+      </div>
+    </>
+  );
 }
