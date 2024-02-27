dd67b738d7b07f14620af39bab8bdb1badd751f9
avr87 <annavanruiten@gmail.com>
2024-02-27 10:25:25 +0000

    added styling for registration page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.css";
 import "@/app.css";
-import Image from "next/legacy/image";
 import background from "../../src/background.png";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,24 +8,15 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div
-          className="background"
-          style={{
-            zIndex: "auto",
-            position: "fixed",
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <Image
-            src={background}
-            placeholder="blur"
-            layout="fill"
-            objectFit="cover"
-            alt="background image"
-          />
-        </div>
+      <body
+        className={inter.className}
+        style={{
+          backgroundImage: `url(${background.src})`,
+          backgroundSize: "cover",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
         {children}
       </body>
     </html>
